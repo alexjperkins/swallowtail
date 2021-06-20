918a7e14e516f08733c0d1390316a5a12ac8f753
alexjperkins <alexjperkins@hotmail.co.uk>
2021-06-20 18:35:35 +0000

    chore: update pager type

--- s.account/pager/registry.go
+++ s.account/pager/registry.go
@@ -2,18 +2,19 @@ package pager
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 
 	"github.com/monzo/terrors"
+
+	accountproto "swallowtail/s.account/proto"
 )
 
 var (
-	pagers  = map[int]Pager{}
+	pagers  = map[accountproto.PagerType]Pager{}
 	pagerMu sync.RWMutex
 )
 
-func register(id int, pager Pager) {
+func register(id accountproto.PagerType, pager Pager) {
 	pagerMu.Lock()
 	defer pagerMu.Unlock()
 
@@ -25,13 +26,13 @@ func register(id int, pager Pager) {
 }
 
 // GetPagerByID is a concurrency safe way to retrieve the given pager by ID
-func GetPagerByID(id int) (Pager, error) {
+func GetPagerByID(id accountproto.PagerType) (Pager, error) {
 	pagerMu.RLock()
 	defer pagerMu.RUnlock()
 	pager, ok := pagers[id]
 	if !ok {
 		return nil, terrors.BadRequest("invalid-pager-id", "Failed to retreive pager; doesn't exist", map[string]string{
-			"pager_id": strconv.Itoa(id),
+			"pager_id": string(id),
 		})
 	}
 	return pager, nil
