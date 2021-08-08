a63b43582b75cd8b7801c6257f3ef9a9ada6a44b
alexjperkins <alexjperkins@hotmail.co.uk>
2021-08-08 10:47:35 +0000

    add gerrors

---
+++ libraries/gerrors/codes.go
@@ -0,0 +1,16 @@
+package gerrors
+
+import "google.golang.org/grpc/codes"
+
+const (
+	ErrFailedPrecondition = codes.FailedPrecondition
+	ErrNotFound           = codes.NotFound
+	ErrUnauthenticated    = codes.Unauthenticated
+	ErrAlreadyExists      = codes.AlreadyExists
+	ErrUnknown            = codes.Unknown
+	ErrUnimplemented      = codes.Unimplemented
+	ErrUnavailable        = codes.Unavailable
+	ErrPermissionDenied   = codes.PermissionDenied
+	ErrCanceled           = codes.Canceled
+	ErrDeadlineExceeded   = codes.DeadlineExceeded
+)
